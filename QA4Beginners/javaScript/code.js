4a94cd5e661cc770a98575181b264bac9eea1309
Md. Sadiqul Alam Saimon <asadiqulsaimon@gmail.com>
2023-05-11 10:50:20 +0600

    first

---
+++ QA4Beginners/javaScript/code.js
@@ -0,0 +1,1 @@
+document.getElementById("texty").innerHTML = "Hello there!"
---
+++ QA4Beginners/javaScript/new.js
@@ -0,0 +1,49 @@
+javaScript
+
+//Data Types
+
+//Strings. Example: "Hello", "I", "1"
+
+//Numbers: Example: 1, 100, 0.5
+
+//Bool: true, false
+
+
+//x + 1 = 3
+//where x = 2
+
+// Arithmatic: + - / *
+// Assignment: =
+// Comparison: < > <= >=
+// != == ===
+
+// Logical: || &&
+
+// if statements
+
+if (1 + 1 == 2) {
+    console.log("That is correct")
+}
+
+// While Loops
+
+while (i < 100) {
+    //do something
+    // i = i + 1
+}
+
+// for loop
+
+for (var i = 0; i < 10; i++) {
+
+}
+
+// Functions
+
+function sayHello (word) {
+    console.log(word);
+}
+
+if(1+1==2){
+    console.log("that is correct") 
+} else if {"that is incorrect"}
